a5917592c8b241500611b1ed7d6ddbc65f41fb8d
Zen Tang <ztang230@gmail.com>
2021-12-25 23:08:09 +0000

    Get Profile on the client side

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { myScript } from "../lib/config";
+import { getProfile } from "../lib/config";
+import * as fcl from "@onflow/fcl";
 
 export default function Home() {
   return (
@@ -22,14 +23,38 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <button
-          onClick={async () => {
-            let c = await myScript();
-            console.log("Result", c);
-          }}
-        >
-          Click me!
-        </button>
+        <div styles={{ display: "flex" }}>
+          <button
+            onClick={async () => {
+              let c = await fcl.authenticate();
+              console.log("Authenticated", c);
+            }}
+          >
+            Log in
+          </button>
+
+          <button
+            onClick={async () => {
+              let c = await fcl.unauthenticate();
+              console.log("Logged out", c);
+            }}
+          >
+            Log out
+          </button>
+
+          <button
+            onClick={async () => {
+              await fcl.authenticate();
+              let currentUser = await fcl.currentUser().snapshot();
+              console.log("The Current User", currentUser);
+
+              let c = await getProfile(currentUser);
+              console.log("getProfile", c);
+            }}
+          >
+            Get my Profile
+          </button>
+        </div>
       </main>
 
       <footer className={styles.footer}>
