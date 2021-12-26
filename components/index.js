be92daeb7ddb3c8244dc9002fa4aeaa93ec3c181
Zen Tang <ztang230@gmail.com>
2021-12-26 20:54:54 +0000

    mobile styles

--- components/index.js
+++ components/index.js
@@ -11,9 +11,9 @@ export function Button({ children, onClick }) {
 
 export function ButtonGroup({ children, name }) {
   return (
-    <div className="flex flex-col gap-4 p-4 rounded-xl bg-white border">
+    <div className="flex flex-col gap-4 p-4 rounded-xl bg-white border w-full max-w-prose">
       <p className="text-gray-500 font-medium">{name}</p>
-      <div className="flex gap-4 items-start">{children}</div>
+      <div className="flex flex-col lg:flex-row gap-4 items-start flex-wrap">{children}</div>
     </div>
   );
 }
